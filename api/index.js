7d0890e2b5a768809f66ea9385e296882d89d704
n4n5 <its.just.n4n5@gmail.com>
2022-02-23 16:16:59 +0100

    [fix]

---
+++ api/index.js
@@ -0,0 +1,24 @@
+import showdown from 'showdown';
+import showdownKatex from "showdown-katex";
+import showdownHighlight from "showdown-highlight";
+
+const converter = new showdown.Converter({
+    openLinksInNewWindow: true,
+    extensions: [
+        showdownKatex({
+            throwOnError: true,
+            displayMode: false,
+        }),
+        showdownHighlight()
+    ],
+});
+converter.setFlavor("github");
+
+
+module.exports = async (req, res) => {
+    if (req && req.body) {
+        const result = converter.makeHtml(req.body);
+        res.end(result);
+    }
+    res.end("");
+}
